692e831f7c43e95ef3235512bfb4a202809109a4
ByeongKeon Kim <skysign@gmail.com>
2021-08-31 21:44:18 +0900

    Baseline before to add API

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,20 @@
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
+console.log(handleSubmit);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
+
+// console.log(checkForName);
+//
+// alert("I EXIST")
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+// console.log(dirname)
+// app.use(express.static('src/client'));
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+let port = 8081;
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`);
+})
+
+app.get('/nlp', function (req, res) {
+    res.send('nlp');
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    console.log('test');
+    res.send(mockAPIResponse);
 })
+
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
+const apiKey = process.env.API_KEY
+console.log(`Your API Key is ${process.env.API_KEY}`);
